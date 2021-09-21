7a9a8b5507f392aaa34dce6dc32a7993a048095e
Iliass Ben <64285535+Bnilss@users.noreply.github.com>
2021-09-21 22:13:20 +0200

    Update and rename validate.py to predict.py

--- predict.py
+++ predict.py
@@ -175,14 +175,14 @@ def main() -> None:
     total_spectrum = 0.0
 
     # Get a list of test image file names.
-    filenames = os.listdir(lr_dir)
+    filenames = os.listdir(lr_dir) if os.path.isdir(lr_dir) else [lr_dir]
     # Get the number of test image files.
     total_files = len(filenames)
-
+    print("Processing", total_files, "file(s) ..")
     for index in range(total_files):
         lr_path = os.path.join(lr_dir, filenames[index])
         sr_path = os.path.join(sr_dir, filenames[index])
-        hr_path = os.path.join(hr_dir, filenames[index])
+        hr_path = os.path.join(hr_dir, filenames[index]) if hr_dir else None
         # Process low-resolution images into super-resolution images.
         lr_image = Image.open(lr_path)
         lr_tensor = image2tensor(lr_image).to(device).unsqueeze(0)
@@ -192,16 +192,17 @@ def main() -> None:
             torchvision.utils.save_image(sr_tensor, sr_path)
 
         # Test the image quality difference between the super-resolution image and the original high-resolution image.
-        print(f"Processing `{os.path.abspath(lr_path)}`...")
-        psnr, ssim, spectrum = image_quality_assessment(sr_path, hr_path)
-        total_psnr += psnr
-        total_ssim += ssim
-        total_spectrum += spectrum
-
-    print(f"PSNR:     {total_psnr / total_files:.2f}.\n"
-          f"SSIM:     {total_ssim / total_files:.4f}.\n"
-          f"Spectrum: {total_spectrum / total_files:.6f}.\n")
-
+        if hr_path:
+            print(f"Processing `{os.path.abspath(lr_path)}`...")
+            psnr, ssim, spectrum = image_quality_assessment(sr_path, hr_path)
+            total_psnr += psnr
+            total_ssim += ssim
+            total_spectrum += spectrum
+    if hr_dir:
+        print(f"PSNR:     {total_psnr / total_files:.2f}.\n"
+            f"SSIM:     {total_ssim / total_files:.4f}.\n"
+            f"Spectrum: {total_spectrum / total_files:.6f}.\n")
+    print("Done.")
 
 if __name__ == "__main__":
     main()
